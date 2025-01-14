8c940d790a04134c666007d44bdca2ec689a27a8
varyansan <varyan.san.98@gmail.com>
2025-01-14 16:01:52 +0300

    create

---
+++ src/main/java/com/hogwarts/demo/HogwartsHwApplication.java
@@ -0,0 +1,13 @@
+package com.hogwarts.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsHwApplication.class, args);
+	}
+
+}
